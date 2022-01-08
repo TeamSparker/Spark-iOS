d58a7afd55953f20f81767ade9d7b0b84a13daad
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-09 05:38:46 +0900

    [Chore] #7 - UIColor+Extension 코드 작성
    제플린에서 UIColor+Extension 코드를 가져왔습니다.

--- Spark-iOS/Spark-iOS/Source/Extensions/UIColor+Extension.swift
+++ Spark-iOS/Spark-iOS/Source/Extensions/UIColor+Extension.swift
@@ -9,5 +9,40 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    
+    @nonobjc class var sparkDarkPinkred: UIColor {
+        return UIColor(red: 1.0, green: 0.0, blue: 61.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkPinkred: UIColor {
+        return UIColor(red: 1.0, green: 34.0 / 255.0, blue: 87.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkBlack: UIColor {
+        return UIColor(white: 0.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkDeepGray: UIColor {
+        return UIColor(white: 43.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkDarkGray: UIColor {
+        return UIColor(red: 115.0 / 255.0, green: 115.0 / 255.0, blue: 118.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkGray: UIColor {
+        return UIColor(red: 177.0 / 255.0, green: 178.0 / 255.0, blue: 180.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkMostLightPinkred: UIColor {
+        return UIColor(red: 1.0, green: 213.0 / 255.0, blue: 223.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkMoreLightPinkred: UIColor {
+        return UIColor(red: 1.0, green: 184.0 / 255.0, blue: 201.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkLightPinkred: UIColor {
+        return UIColor(red: 1.0, green: 137.0 / 255.0, blue: 165.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkLightGray: UIColor {
+        return UIColor(red: 237.0 / 255.0, green: 237.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkBrightPinkred: UIColor {
+        return UIColor(red: 1.0, green: 81.0 / 255.0, blue: 123.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var sparkWhite: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
 }
