2a073abb731a50c015b4eab437872c915da5e725
hyungyu Kim <ypsophila97@icloud.com>
2022-01-13 07:47:49 +0900

    [Chore] #35 - 본 프로젝트에 맞게 UINavigationController extension 구현

--- Spark-iOS/Spark-iOS/Source/Extensions/UINavigationController+.swift
+++ Spark-iOS/Spark-iOS/Source/Extensions/UINavigationController+.swift
@@ -11,8 +11,7 @@ import UIKit
 
 extension UINavigationController {
     
-    // MARK: - 투명
-    
+    /// 투명
     func initTransparentNavBar() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
@@ -20,67 +19,41 @@ extension UINavigationController {
         self.navigationBar.scrollEdgeAppearance = appearance
     }
     
-    // MARK: - 뒤로가기 버튼
-    
-    func initWithBackButton() {
+    /// 뒤로가기 버튼 + 제목.
+    /// - title 에는 네비게이션바 타이틀을 넣어주세요.
+    func initWithBackButton(title: String) {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
         appearance.initBackButtonAppearance()
         
         self.navigationBar.standardAppearance = appearance
         self.navigationBar.scrollEdgeAppearance = appearance
-        self.navigationBar.tintColor = .black
+        
+        self.navigationController?.title = title
     }
     
-    // MARK: - 뒤로가기 버튼 + 완료 버튼
-    
-    func initWithBackAndDoneButton(navigationItem: UINavigationItem?, doneButtonClosure: Selector) {
-        initWithBackButton()
+    /// 홈 우측 버튼 두개.
+    /// - navigationItem 에는 해당 뷰컨트롤러의 네비게이션바를 넣어주세요
+    /// - clouser 에는 해당 버튼이 눌렸을 때 화면전환을 넣어주세요
+    func initWithTwoCustomButtons(navigationItem: UINavigationItem?, firstButtonClosure: Selector, secondButtonClosure: Selector) {
+        initWithBackButton(title: "")
         
-        // done Button
-        let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self.topViewController, action: doneButtonClosure)
-        navigationItem?.rightBarButtonItem = doneButton
-    }
-    
-    // MARK: - 커스텀 버튼 1개
-    
-    func initWithOneCustomButtons(navigationItem: UINavigationItem?, firstButtonImage: UIImage, firstButtonClosure: Selector) {
-        initWithBackButton()
-        
-        let firstButton = UIBarButtonItem(image: firstButtonImage, style: .plain, target: self.topViewController, action: firstButtonClosure)
-        navigationItem?.rightBarButtonItem = firstButton
-        
-    }
-    
-    // MARK: - 커스텀 버튼 2개
-    
-    func initWithTwoCustomButtons(navigationItem: UINavigationItem?, firstButtonImage: UIImage, secondButtonImage: UIImage, firstButtonClosure: Selector, secondButtonClosure: Selector) {
-        initWithBackButton()
-        
-        let firstButton = UIBarButtonItem(image: firstButtonImage, style: .plain, target: self.topViewController, action: firstButtonClosure)
-        let secondButton = UIBarButtonItem(image: secondButtonImage, style: .plain, target: self.topViewController, action: secondButtonClosure)
+        let firstButton = UIBarButtonItem(image: UIImage(named: "icProfile"), style: .plain, target: self.topViewController, action: firstButtonClosure)
+        let secondButton = UIBarButtonItem(image: UIImage(named: "icNotice"), style: .plain, target: self.topViewController, action: secondButtonClosure)
         navigationItem?.rightBarButtonItems = [firstButton, secondButton]
-        
-    }
-    
-    // MARK: - @objc function
-    
-    @objc func touchBackButton() {
-        popViewController(animated: true)
     }
 }
 
 extension UINavigationBarAppearance {
-    
     func initBackButtonAppearance() {
-        // back button image
         var backButtonImage: UIImage? {
-            return UIImage(systemName: "chevron.left")?.withAlignmentRectInsets(UIEdgeInsets(top: 0.0, left: -12.0, bottom: -5.0, right: 0.0))
+//            return UIImage(named: "icBackWhite")?.withAlignmentRectInsets(UIEdgeInsets(top: 0.0, left: -12.0, bottom: -5.0, right: 0.0))
+            return UIImage(named: "icBackWhite")
         }
-        // back button appearance
+
         var backButtonAppearance: UIBarButtonItemAppearance {
             let backButtonAppearance = UIBarButtonItemAppearance()
-            // backButton하단에 표출되는 text를 안보이게 설정
+            // backButton 옆에 표출되는 text를 안보이게 설정
             backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear, .font: UIFont.systemFont(ofSize: 0.0)]
 
             return backButtonAppearance
