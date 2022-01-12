2048a36b5511405c231c181bebcfc5fae7d8dc0a
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-13 07:33:13 +0900

    [Add] #36 - 인증사진 모아보기 뷰 파일 세팅
    인증사진 모아보기 뷰 파일 세팅

---
+++ Spark-iOS/Spark-iOS/Source/Cells/StorageMoreCVC/MoreStorageCVC.swift
@@ -0,0 +1,17 @@
+//
+//  MoreStorageCVC.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/13.
+//
+
+import UIKit
+
+class MoreStorageCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/StorageMore/StorageMoreVC.swift
@@ -0,0 +1,29 @@
+//
+//  StorageMoreVC.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/13.
+//
+
+import UIKit
+
+class StorageMoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
