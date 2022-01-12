4507ecfb273f1f9a46e4bc4f65a511181e7637c4
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-13 06:08:02 +0900

    [Feat] #24 - UIView 그라데이션 함수 추가
    UIView 그라데이션 함수 추가

--- Spark-iOS/Spark-iOS/Source/Cells/StorageCVC/DoingStorageCVC.swift
+++ Spark-iOS/Spark-iOS/Source/Cells/StorageCVC/DoingStorageCVC.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class DoingStorageCVC: UICollectionViewCell {
-
+    
+    @IBOutlet weak var gradationUIView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUIView()
     }
 
+    func setUIView() {
+        gradationUIView.setGradient(color1: UIColor.clear, color2: UIColor.black)
+    }
+}
+
+extension UIView{
+    func setGradient(color1:UIColor,color2:UIColor){
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [color1.cgColor,color2.cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.8)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
 }
