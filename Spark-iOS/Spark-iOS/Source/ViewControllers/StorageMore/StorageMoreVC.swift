8b65e52ebbbe1b223c21fbb29692b479967c93a3
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-13 08:14:14 +0900

    [Feat] #36 - MoreStorageCV 뼈대 구현
    MoreStorageCV 뼈대 구현

--- Spark-iOS/Spark-iOS/Source/ViewControllers/StorageMore/StorageMoreVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/StorageMore/StorageMoreVC.swift
@@ -9,21 +9,53 @@ import UIKit
 
 class StorageMoreVC: UIViewController {
 
+    var storageMoreCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10
+        
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let name = UICollectionView(frame: CGRect(x: 0,y: 197,width: 375,height: 520), collectionViewLayout: layout)
+        name.backgroundColor = .blue
+        
+        return name
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(storageMoreCV)
+        setDelegate()
+        registerXib()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setDelegate() {
+        storageMoreCV.delegate = self
+        storageMoreCV.dataSource = self
     }
-    */
+    
+    func registerXib() {
+        let xibCollectionViewName = UINib(nibName: "MoreStorageCVC", bundle: nil)
+        storageMoreCV.register(xibCollectionViewName, forCellWithReuseIdentifier: "MoreStorageCVC")
+    }
 
+}
+
+extension StorageMoreVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 160, height:197)
+    }
+}
+
+extension StorageMoreVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoreStorageCVC", for: indexPath) as? MoreStorageCVC else {return UICollectionViewCell()}
+
+        return cell
+    }
 }
