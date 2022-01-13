8e72e4293ba6901bba9e05e6746d03327d6ed1c0
hyungyu Kim <ypsophila97@icloud.com>
2022-01-13 23:47:27 +0900

    [Feat] #34 - HomeVC 레이아웃 및 셀 구현

---
+++ Spark-iOS/Spark-iOS/Source/Cells/Home/HomeWaitingCVC.swift
@@ -0,0 +1,44 @@
+//
+//  HomeWaitingCVC.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/01/13.
+//
+
+import UIKit
+
+class HomeWaitingCVC: UICollectionViewCell {
+
+    @IBOutlet weak var flakeImage: UIImageView!
+    @IBOutlet weak var ticketImage: UIImageView!
+    @IBOutlet weak var habitTitleLabel: UILabel!
+    
+    // MARK: - View Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
+        setUI()
+    }
+    
+    override func prepareForReuse() {
+        habitTitleLabel.text = ""
+        flakeImage.image = UIImage()
+        ticketImage.image = UIImage()
+    }
+}
+
+// MARK: - Methods
+
+extension HomeWaitingCVC {
+    private func setUI() {
+        habitTitleLabel.font = .h2Title
+        habitTitleLabel.textColor = .sparkDeepGray
+    }
+    
+    // TODO: - initCell
+    
+    func initCell() {
+        
+    }
+}
