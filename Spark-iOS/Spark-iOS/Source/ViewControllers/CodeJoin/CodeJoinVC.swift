e2d07ec590da54d9bb1dd970517a52b4b5ec3ad5
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-14 06:57:47 +0900

    [Feat] #43 - 화면전환 기능 구현
    버튼 클릭 시 화면전환 기능 구현

--- Spark-iOS/Spark-iOS/Source/ViewControllers/CodeJoin/CodeJoinVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/CodeJoin/CodeJoinVC.swift
@@ -10,14 +10,32 @@ import UIKit
 class CodeJoinVC: UIViewController {
 
     @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var okView: UIView!
+    @IBOutlet weak var okButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setAddTargets()
     }
     
     func setUI() {
         view.backgroundColor = .sparkBlack.withAlphaComponent(0.8)
         tabBarController?.tabBar.isHidden = true
         popUpView.layer.cornerRadius = 2
+    }
+    
+    func setAddTargets() {
+        okButton.addTarget(self, action: #selector(touchOkayButton), for:. touchUpInside)
+    }
+
+    @objc func touchOkayButton() {
+        let nextSB = UIStoryboard.init(name: "JoinCheck", bundle:nil)
+        
+        guard let nextVC = nextSB.instantiateViewController(identifier: "JoinCheckVC") as? JoinCheckVC else {return}
+        
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: false, completion: nil)
     }
 }
