e75902aa520d1b9acf6879c90245dd69b1408d19
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-14 06:58:20 +0900

    [Design] #43 - JoinCheckVC 레이아웃, UI 구성
    JoinCheckVC 레이아웃, UI 구성

--- Spark-iOS/Spark-iOS/Source/ViewControllers/CodeJoin/JoinCheckVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/CodeJoin/JoinCheckVC.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class JoinCheckVC: UIViewController {
 
+    @IBOutlet weak var reInputButton: UIButton!
+    @IBOutlet weak var enterButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setUI()
+    }
+    
+    func setUI() {
+        reInputButton.setTitleColor(.sparkDarkPinkred, for: .normal)
+        reInputButton.layer.borderColor = .init(_colorLiteralRed: 255, green: 0, blue: 11, alpha: 1)
+        reInputButton.layer.borderWidth = 1
+        enterButton.setTitleColor(.white, for: .normal)
+        enterButton.backgroundColor = .sparkDarkPinkred
+        enterButton.titleLabel?.font = .btn1Default
+        enterButton.titleLabel?.textColor = .sparkWhite
+        enterButton.layer.borderWidth = 0
     }
 }
