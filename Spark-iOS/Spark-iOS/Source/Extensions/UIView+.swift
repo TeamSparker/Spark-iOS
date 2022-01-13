6a071dc719871be14e3edfcc2431e1f4dd30a4fc
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-13 22:05:54 +0900

    [Chore] #36 - gradient 함수 위치 이동
    gradient 함수 UIView 익스텐션으로 이동

--- Spark-iOS/Spark-iOS/Source/Extensions/UIView+.swift
+++ Spark-iOS/Spark-iOS/Source/Extensions/UIView+.swift
@@ -12,3 +12,15 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
 }
+
+extension UIView{
+    func setGradient(color1:UIColor,color2:UIColor){
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [color1.cgColor,color2.cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.8)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
+}
