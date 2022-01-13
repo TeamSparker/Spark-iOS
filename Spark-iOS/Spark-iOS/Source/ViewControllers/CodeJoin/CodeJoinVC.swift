b6345fa5f65d6f21a449781e7f9178d46e2c2418
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-14 00:55:45 +0900

    [Add] #43 - 코드로 참여하기 스토리보드 생성
    코드로 참여하기 스토리보드, 뷰컨트롤러 생성

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/CodeJoin/CodeJoinVC.swift
@@ -0,0 +1,15 @@
+//
+//  CodeJoinVC.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/14.
+//
+
+import UIKit
+
+class CodeJoinVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
