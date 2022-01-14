5c178e95265a2fe622bc592c092dd47c2b30711b
yangsubinn <cindy010670@gmail.com>
2022-01-14 11:57:52 +0900

    [Add] #46 - CreateAuth에서 쓰이는 View 추가 및 TempView

---
+++ Spark-iOS/Spark-iOS/Source/Views/CreateAuth/PhotoAuthView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoAuthView.swift
+//  Spark-iOS
+//
+//  Created by 양수빈 on 2022/01/14.
+//
+
+import UIKit
+
+class PhotoAuthView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Spark-iOS/Spark-iOS/Source/Views/CreateAuth/TimerAuthView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerAuthView.swift
+//  Spark-iOS
+//
+//  Created by 양수빈 on 2022/01/14.
+//
+
+import UIKit
+
+class TimerAuthView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
