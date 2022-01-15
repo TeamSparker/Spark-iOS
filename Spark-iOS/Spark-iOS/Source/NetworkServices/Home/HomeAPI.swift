9300d7cf0f25cc3cb430a09564552361e970084e
hyungyu Kim <ypsophila97@icloud.com>
2022-01-15 15:42:22 +0900

    [Feat] #49 - Moya 세팅 및 예시 서버통신 구현
    - HomeAPI, HomeService
    - 습관방 리스트 조회 서버 구현

--- Spark-iOS/Spark-iOS/Source/NetworkServices/Home/HomeAPI.swift
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/Home/HomeAPI.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import Moya
+
+public class UserAPI {
+     
+    static let shared = UserAPI()
+    var userProvider = MoyaProvider<HomeService>(plugins: [MoyaLoggerPlugin()])
+    
+    public init() { }
+    
+    func habitRoomFetch(lastID: Int, size: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        userProvider.request(.habitRoomFetch(lastID: lastID, size: size)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+
+                let networkResult = self.judgeHabitRoomFetchStatus(by: statusCode, data)
+                completion(networkResult)
+
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    private func judgeHabitRoomFetchStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<HabitRoom>.self, from: data)
+        else {
+            return .pathErr
+        }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400..<500:
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    /// 응답 시 data 가 없는 경우.
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<String>.self, from: data)
+        else { return .pathErr }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.message)
+        case 400..<500:
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
--- Spark-iOS/Spark-iOS/Source/NetworkServices/Home/HomeService.swift
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/Home/HomeService.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Moya
+
+enum HomeService {
+    case habitRoomFetch(lastID: Int, size:Int)
+}
+
+extension HomeService: TargetType {
+    var baseURL: URL {
+        return URL(string: Const.URL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .habitRoomFetch:
+            return "/room"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .habitRoomFetch:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .habitRoomFetch(let lastID, let size):
+            return .requestParameters(parameters: ["lastid": lastID,
+                                                   "size": size],
+                                      encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .habitRoomFetch:
+            return Const.Header.authrizationHeader
+        }
+    }
+    
+    
+}
