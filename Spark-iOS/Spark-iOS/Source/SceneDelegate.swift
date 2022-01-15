aebabe1abfdb8af36fd59b763c9c76fb7b19c64b
yangsubinn <cindy010670@gmail.com>
2022-01-16 02:48:06 +0900

    [Design] #60 - ResetPopup 레이아웃

--- Spark-iOS/Spark-iOS/Source/SceneDelegate.swift
+++ Spark-iOS/Spark-iOS/Source/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        let rootViewController = UIStoryboard(name: Const.Storyboard.Name.authTimer, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.authTimer)
+        let rootViewController = UIStoryboard(name: Const.Storyboard.Name.resetPopup, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.resetPopup)
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
