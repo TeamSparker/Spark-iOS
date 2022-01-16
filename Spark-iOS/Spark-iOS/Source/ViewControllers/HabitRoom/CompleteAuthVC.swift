a6516c08275e548c3e81beb80eca80a9e2decd67
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-16 13:28:19 +0900

    [Design] #70 - AuthComplete UI 레이아웃 구성
     AuthComplete UI 레이아웃 구성

--- Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/CompleteAuthVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/CompleteAuthVC.swift
@@ -9,16 +9,35 @@ import UIKit
 
 class CompleteAuthVC: UIViewController {
 
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var instaView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setGesture()
     }
 
+    @IBAction func touchNoDismiss(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
 }
 
 extension CompleteAuthVC {
     private func setUI() {
         view.backgroundColor = .sparkBlack.withAlphaComponent(0.8)
         tabBarController?.tabBar.isHidden = true
+        
+        instaView.layer.cornerRadius = 2
+    }
+    
+    private func setGesture() {
+        let instaTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
+        instaView.addGestureRecognizer(instaTapGesture)
+    }
+    
+    @objc
+    func tapped(_ gesture: UITapGestureRecognizer) {
+        // 인스타 공유 기능
     }
 }
