4185d69b59664f22c1c449c41e6d19dcd5444412
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-16 16:17:57 +0900

    [Fix] #70 - 코드리뷰 반영
    코드리뷰 반영

--- Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/HabitAuthVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/HabitAuthVC.swift
@@ -47,18 +47,18 @@ extension HabitAuthVC {
         
         // TODO: 고민중, 쉴래요 버튼 기능 구현
         considerButton.setTitleColor(.sparkLightPinkred, for: .highlighted)
-        considerButton.layer.borderColor = .init(_colorLiteralRed: 1, green: 137/255, blue: 165/255, alpha: 1)
+        considerButton.layer.borderColor = UIColor.sparkLightGray.cgColor
         considerButton.layer.borderWidth = 1
         considerButton.layer.cornerRadius = 2
         
         restButton.setTitleColor(.sparkLightPinkred, for: .highlighted)
-        restButton.layer.borderColor = .init(_colorLiteralRed: 1, green: 137/255, blue: 165/255, alpha: 1)
+        restButton.layer.borderColor = UIColor.sparkLightGray.cgColor
         restButton.layer.borderWidth = 1
         restButton.layer.cornerRadius = 2
     }
     
     private func setAddTargets() {
-        okButton.addTarget(self, action: #selector(touchOkayButton), for:. touchUpInside)
+        okButton.addTarget(self, action:#selector(touchOkayButton),  for: .touchUpInside)
     }
     
     private func setDelegate() {
@@ -70,17 +70,17 @@ extension HabitAuthVC {
         alter.view.tintColor = .sparkBlack
         
         /// alter에 들어갈 액션 생성
-        let library = UIAlertAction(title: "카메라 촬영", style: .default) { _ in
+        let camera = UIAlertAction(title: "카메라 촬영", style: .default) { _ in
             self.openCamera()
         }
-        let camera = UIAlertAction(title: "앨범에서 선택하기", style: .default) { _ in
+        let library = UIAlertAction(title: "앨범에서 선택하기", style: .default) { _ in
             self.openLibrary()
         }
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
         
         /// alter에 액션을 넣어줌
-        alter.addAction(library)
         alter.addAction(camera)
+        alter.addAction(library)
         alter.addAction(cancel)
         
         /// button tap했을 때 alter present
