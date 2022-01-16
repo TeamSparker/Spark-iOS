46506c4c39ec0448d0fda772b844a52ddd3917f2
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-17 02:27:32 +0900

    [Add] #82 - 보관함 네트워크 기초 세팅
    보관함 네트워크 기초 세팅

---
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/MyRoom/MyRoomAPI.swift
@@ -0,0 +1,8 @@
+//
+//  MyRoomAPI.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/17.
+//
+
+import Foundation
---
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/MyRoom/MyRoomService.swift
@@ -0,0 +1,8 @@
+//
+//  MyRoomService.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/17.
+//
+
+import Foundation
