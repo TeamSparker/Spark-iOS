f62cdd354349e6bd8f0fccd5dd595e605b84c0eb
hyungyu Kim <ypsophila97@icloud.com>
2022-01-17 11:20:37 +0900

    [Add] #83 - HomeEmptyCVC 구현

---
+++ Spark-iOS/Spark-iOS/Source/Cells/Home/HomeEmptyCVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeEmptyCVC.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/01/17.
+//
+
+import UIKit
+
+class HomeEmptyCVC: UICollectionViewCell {
+    
+    // MARK: - View Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
