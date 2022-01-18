c0e22a23d4ab0827cdaddd5064a0565b23771797
yangsubinn <cindy010670@gmail.com>
2022-01-19 03:19:59 +0900

    [Design] #115 - 프로필 설정 뷰 레이아웃

--- Spark-iOS/Spark-iOS/Source/ViewControllers/Profile/ProfileSettingVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Profile/ProfileSettingVC.swift
@@ -16,8 +16,8 @@ class ProfileSettingVC: UIViewController {
     let closeButton = UIButton()
     let titleLabel = UILabel()
     let profileImageView = UIImageView()
-    let fadeImageView = UIImageView()
-    let photoImageView = UIImageView()
+    let fadeView = UIView()
+    let photoIconImageView = UIImageView()
     let textField = UITextField()
     let lineView = UIView()
     let countLabel = UILabel()
@@ -25,17 +25,101 @@ class ProfileSettingVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
     }
     
+    private func setUI() {
+        closeButton.setImage(UIImage(named: "icQuit"), for: .normal)
+        titleLabel.text = "스파크에서 사용할 \n프로필을 설정해주세요!"
+        titleLabel.font = .h2Title
+        titleLabel.textColor = .sparkBlack
+        titleLabel.numberOfLines = 2
+        titleLabel.partColorChange(targetString: "프로필", textColor: .sparkPinkred)
+        
+        profileImageView.backgroundColor = .blue
+        profileImageView.image = UIImage(named: "profileEmpty")
+        profileImageView.layer.cornerRadius = 58
+        profileImageView.layer.masksToBounds = true
+        
+        fadeView.backgroundColor = .sparkBlack.withAlphaComponent(0.6)
+        fadeView.layer.cornerRadius = 58
+        
+        photoIconImageView.image = UIImage(named: "icImage")
+        
+        textField.borderStyle = .none
+        textField.placeholder = "닉네임 입력"
+//        textField.delegate = self
 
-    /*
-    // MARK: - Navigation
+        lineView.backgroundColor = .sparkGray
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        countLabel.text = "0/15"
+        countLabel.font = .p2SubtitleEng
+        countLabel.textColor = .sparkDarkGray
+        
+        completeButton.layer.cornerRadius = 2
+        completeButton.titleLabel?.font = .enBoldFont(ofSize: 18)
+        completeButton.setTitle("가입 완료", for: .normal)
+        completeButton.backgroundColor = .sparkGray
+        completeButton.isEnabled = false
     }
-    */
+}
 
+// MARK: - Layout
+extension ProfileSettingVC {
+    private func setLayout() {
+        view.addSubviews([closeButton, titleLabel, profileImageView,
+                         fadeView, textField, lineView,
+                          countLabel, completeButton])
+        fadeView.addSubview(photoIconImageView)
+        
+        closeButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(closeButton.snp.bottom).offset(20)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(188)
+            make.width.height.equalTo(115)
+        }
+        
+        fadeView.snp.makeConstraints { make in
+            make.edges.equalTo(profileImageView)
+        }
+        
+        photoIconImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(55)
+            make.leading.equalToSuperview().inset(20)
+            make.width.equalTo(290)
+            make.height.equalTo(46)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(textField.snp.centerY)
+            make.trailing.equalToSuperview().inset(28)
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(textField.snp.bottom)
+            make.height.equalTo(2)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.width.equalToSuperview().inset(20)
+            make.height.equalTo(self.view.frame.width*48/335)
+        }
+    }
 }
