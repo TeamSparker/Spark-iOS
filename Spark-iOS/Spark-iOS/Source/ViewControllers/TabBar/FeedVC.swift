0265fd0bc51372d8cffdd0242ac75c39c578fec1
yangsubinn <cindy010670@gmail.com>
2022-01-18 21:57:22 +0900

    [Chore] #109 - 피드뷰 서버 연결 주석 추가

--- Spark-iOS/Spark-iOS/Source/ViewControllers/TabBar/FeedVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/TabBar/FeedVC.swift
@@ -33,8 +33,16 @@ class FeedVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setLayout()
         setCollectionView()
+        
+        // FIXME: - getFeedListFetchWithAPI 위치 변경
+        DispatchQueue.main.async {
+            self.getFeedListFetchWithAPI(lastID: self.feedLastID) {
+//                self.collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .bottom, animated: false)
+            }
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -42,11 +50,8 @@ class FeedVC: UIViewController {
         
         NotificationCenter.default.post(name: .disappearFloatingButton, object: nil)
         
-        DispatchQueue.main.async {
-            self.getFeedListFetchWithAPI(lastID: self.feedLastID) {
-                self.collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .bottom, animated: false)
-            }
-        }
+        // FIXME: - getFeedListFetchWithAPI를 여기서 호출하는거로 변경
+//        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .bottom, animated: false)
     }
     
     // MARK: - Methods
