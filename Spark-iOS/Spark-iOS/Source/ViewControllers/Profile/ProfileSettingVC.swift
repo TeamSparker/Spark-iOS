6380b354ee49c6a85c3a9dbeccee1c21f918ee17
yangsubinn <cindy010670@gmail.com>
2022-01-19 01:25:57 +0900

    [Add] #115 - ProfileSetting 파일 추가

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Profile/ProfileSettingVC.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileSettingVC.swift
+//  Spark-iOS
+//
+//  Created by 양수빈 on 2022/01/19.
+//
+
+import UIKit
+
+import SnapKit
+
+class ProfileSettingVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    let closeButton = UIButton()
+    let titleLabel = UILabel()
+    let profileImageView = UIImageView()
+    let fadeImageView = UIImageView()
+    let photoImageView = UIImageView()
+    let textField = UITextField()
+    let lineView = UIView()
+    let countLabel = UILabel()
+    let completeButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
