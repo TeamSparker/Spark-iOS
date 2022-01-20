7451b0a95e417c909dfdb2e9224ccdf1e7ef1a74
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-20 20:52:36 +0900

    [Feat] #150 - WaitingToolTip 초기 세팅
    WaitingToolTip 초기 세팅

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Waiting/WaitingToolTipVC.swift
@@ -0,0 +1,16 @@
+//
+//  WaitingToolTipVC.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/20.
+//
+
+import UIKit
+
+class WaitingToolTipVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
