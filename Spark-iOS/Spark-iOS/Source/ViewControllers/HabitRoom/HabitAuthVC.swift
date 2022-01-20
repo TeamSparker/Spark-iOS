bd96b8cec9c31ea33d71a4da6aa850f80006adce
yangsubinn <cindy010670@gmail.com>
2022-01-21 01:10:55 +0900

    [Chore] #139 - 코드 정리 및 사진+시간 인증 플로우 수정

--- Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/HabitAuthVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/HabitRoom/HabitAuthVC.swift
@@ -17,8 +17,6 @@ class HabitAuthVC: UIViewController {
 
     // MARK: - Properties
     
-    private let picker = UIImagePickerController()
-    private var imageContainer = UIImage()
     var authType: AuthType?
     var roomID: Int?
     var rest: Int?
@@ -39,7 +37,6 @@ class HabitAuthVC: UIViewController {
         super.viewDidLoad()
         setUI()
         setAddTargets()
-        setDelegate()
     }
     
     
@@ -92,51 +89,16 @@ extension HabitAuthVC {
         restButton.addTarget(self, action: #selector(touchRestButton), for: .touchUpInside)
     }
     
-    private func setDelegate() {
-        picker.delegate = self
-    }
-    
-    private func showAlert() -> UIViewController {
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alert.view.tintColor = .sparkBlack
-        
-        /// alter에 들어갈 액션 생성
-        let camera = UIAlertAction(title: "카메라 촬영", style: .default) { _ in
-            self.openCamera()
-        }
-        let library = UIAlertAction(title: "앨범에서 선택하기", style: .default) { _ in
-            self.openLibrary()
-        }
-        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-        
-        /// alter에 액션을 넣어줌
-        alert.addAction(camera)
-        alert.addAction(library)
-        alert.addAction(cancel)
-        
-        /// button tap했을 때 alter present
-//        self.modalTransitionStyle = .coverVertical
-        
-//        present(alert, animated: true) {
-//            guard let nextVC = UIStoryboard(name: Const.Storyboard.Name.authUpload, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.authUpload) as? AuthUploadVC else { return }
-//            self.present(nextVC, animated: true, completion: nil)
-//        }
-        
-//        return alert
-//        present(alert, animated: true, completion: nil)
-        return alert
-    }
-    
     @objc
     private func touchOkayButton() {
+        let presentingVC = self.presentingViewController
         switch authType {
         case .photoOnly:
             self.dismiss(animated: true) {
-            self.presentAlertClosure?()
+                self.presentAlertClosure?()
             }
         case .photoTimer:
             self.dismiss(animated: true) {
-                let presentingVC = self.presentingViewController
                 guard let rootVC = UIStoryboard(name: Const.Storyboard.Name.authTimer, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.authTimer) as? AuthTimerVC else { return }
                 let nextVC = UINavigationController(rootViewController: rootVC)
                 nextVC.modalPresentationStyle = .fullScreen
@@ -159,58 +121,6 @@ extension HabitAuthVC {
     private func touchRestButton() {
         setConsiderRestWithAPI(statusType: "REST")
     }
-    
-    private func openLibrary() {
-        /// UIImagePickerController에서 어떤 식으로 image를 pick해올지 -> 앨범에서 픽해오겠다
-        picker.sourceType = .photoLibrary
-        present(picker, animated: false, completion: nil)
-    }
-    
-    private func openCamera() {
-        /// 카메라 촬영 타입이 가능하다면
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            /// UIImagePickerController에서 어떤 식으로 image를 pick해올지 -> 카메라 촬영헤서 픽해오겠다
-            picker.sourceType = .camera
-            present(picker, animated: false, completion: nil)
-        } else {
-            print("카메라 안됩니다.")
-        }
-    }
-}
-
-// MARK: UIImagePickerDelegate
-extension HabitAuthVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            imageContainer = image
-        }
-        dismiss(animated: true) {
-//            self.presentAuthUpload()
-            guard let nextVC = UIStoryboard(name: Const.Storyboard.Name.authUpload, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.authUpload) as? AuthUploadVC else { return }
-            
-            nextVC.roomName = self.roomName
-            nextVC.roomId = self.roomID
-            
-            self.present(nextVC, animated: true, completion: nil)
-        }
-    }
-    
-    // TODO: 케이스 나눠서 화면전환 하기
-//    private func presentAuthUpload() {
-//        let nextSB = UIStoryboard.init(name: Const.Storyboard.Name.authUpload, bundle: nil)
-//
-//        guard let nextVC = nextSB.instantiateViewController(identifier: Const.ViewController.Identifier.authUpload) as? AuthUploadVC else {return}
-//
-//        nextVC.uploadImage = self.imageContainer
-//
-//        nextVC.modalPresentationStyle = .fullScreen
-//        self.present(nextVC, animated: false, completion: nil)
-//    }
 }
 
 // MARK: Network
