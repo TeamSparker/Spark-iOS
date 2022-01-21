76cd3626f6999aaaf2f8e72b9a2a1dbdaad04589
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-21 18:50:46 +0900

    [Feat] #146 - 렌더링 커스텀 뷰 파일 추가
    렌더링 뷰 파일 추가

---
+++ Spark-iOS/Spark-iOS/Source/Views/CompleteAuth/ViewForRender.swift
@@ -0,0 +1,20 @@
+//
+//  ViewForRender.swift
+//  Spark-iOS
+//
+//  Created by Junho Lee on 2022/01/21.
+//
+
+import UIKit
+
+class ViewForRender: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
