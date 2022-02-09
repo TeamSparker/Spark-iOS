4bc27f020f95a120029a4d89e5f9fbe1707a4a1f
yangsubinn <cindy010670@gmail.com>
2022-02-10 03:13:18 +0900

    [Chore] #241 - CreateRoomVC, ProfileSettingVC changeCountLabel 함수 적용

--- Spark-iOS/Spark-iOS/Source/ViewControllers/Create/CreateRoomVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Create/CreateRoomVC.swift
@@ -140,32 +140,7 @@ class CreateRoomVC: UIViewController {
     @objc
     private func textFieldDidChange(_ notification: Notification) {
         if let textField = notification.object as? UITextField {
-            if let text = textField.text {
-                /// 글자가 바뀔 때마다 countLabel 업데이트
-                countLabel.text = "\(text.count)/15"
-                
-                /// 글자수 count 초과한 경우
-                if text.count >= maxLength {
-                    let maxIndex = text.index(text.startIndex, offsetBy: maxLength)
-                    let newString = String(text[text.startIndex..<maxIndex])
-                    textField.text = newString
-                    countLabel.text = "15/15"
-                    countLabel.textColor = .sparkPinkred
-                }
-                
-                /// 글자 있는 경우 색 활성화, 없는 경우 비활성화
-                else if text.count > 0 {
-                    let attributedString = NSMutableAttributedString(string: countLabel.text ?? "")
-                    attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.sparkPinkred, range: ((countLabel.text ?? "") as NSString).range(of: "\(text.count)"))
-                    countLabel.textColor = .sparkDarkGray
-                    countLabel.attributedText = attributedString
-                }
-                
-                /// 그 외 0인 경우
-                else {
-                    countLabel.textColor = .sparkDarkGray
-                }
-            }
+            changeCountLabel(textField: textField, maxLength: maxLength, countLabel: countLabel, lineView: lineView)
         }
     }
     
@@ -196,7 +171,7 @@ extension CreateRoomVC: UITextFieldDelegate {
     
     /// 입력 시작
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        ableButton()
+        lineView.backgroundColor = .sparkPinkred
         return true
     }
     
