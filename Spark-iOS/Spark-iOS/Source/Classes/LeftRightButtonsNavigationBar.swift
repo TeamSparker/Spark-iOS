41ab8f50382add4a4978edad969701b20de16913
hyungyu Kim <ypsophila97@icloud.com>
2022-02-17 18:13:23 +0900

    [Feat] #278 - 커스텀 네비게이션바 만들기
    - LeftRightButtonsNavigationBar: LeftButtonNavigationBar + 우측 버튼

---
+++ Spark-iOS/Spark-iOS/Source/Classes/LeftRightButtonsNavigationBar.swift
@@ -0,0 +1,72 @@
+//
+//  LeftRightButtonsNavigationBar.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/02/17.
+//
+
+import Foundation
+import UIKit
+
+import SnapKit
+
+final class LeftRightButtonsNavigationBar: LeftButtonNavigaitonBar {
+    
+    // MARK: - Properties
+    
+    lazy var rightButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(), for: .normal)
+        button.setTitle("", for: .normal)
+        button.tintColor = .sparkBlack
+        
+        return button
+    }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    
+        setLayout()
+    }
+    
+    // MARK: - Methods
+    
+    private func setLayout() {
+        self.addSubview(leftButton)
+        
+        self.rightButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    /// set image of left button.
+    /// - default image is UIImage().
+    @discardableResult
+    func rightButtonImage(_ image: UIImage?) -> Self {
+        if let image = image {
+            self.rightButton.setImage(image, for: .normal)
+        }
+        
+        return self
+    }
+    
+    /// set tint color of left button
+    /// - default color is sparkBlack.
+    @discardableResult
+    override func tintColor(_ color: UIColor) -> Self {
+        super.tintColor(color)
+        
+        self.rightButton.tintColor = color
+        
+        return self
+    }
+}
