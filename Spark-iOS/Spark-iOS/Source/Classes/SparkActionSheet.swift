834296296c3659f7ffe8434153e520aaf505465a
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-25 19:01:05 +0900

    [Feat] #298 - SparkSection, SparkAction, SparkActionMainStackView 추가
    SparkSection, SparkAction, SparkActionMainStackView 추가

--- Spark-iOS/Spark-iOS/Source/Classes/SparkActionSheet.swift
+++ Spark-iOS/Spark-iOS/Source/Classes/SparkActionSheet.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+import SnapKit
+
+// MARK: - SparkSection class
+
+public class SparkSection {
+    public var actions = [SparkAction]()
+    
+    public init() {}
+}
+
+// MARK: SparkAction Class
+
+public class SparkAction {
+    public var data: String
+    public var buttonType: sparkActionTitleType
+    public var handler: (() -> Void)?
+}
+
+class SparkActionMainStackView: UIStackView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.axis = .vertical
+        self.spacing = 0
+    }
+    
+}
+}
+
+}
