896ec9c3c429be2892e31f4f198df7f969cebcfa
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-26 03:12:39 +0900

    [Chore] #298 - 주석 작업
    주석 작업

--- Spark-iOS/Spark-iOS/Source/Classes/SparkActionSheet.swift
+++ Spark-iOS/Spark-iOS/Source/Classes/SparkActionSheet.swift
@@ -11,12 +11,21 @@ import SnapKit
 
 // MARK: - SparkSection class
 
+/// Section : StackView를 생성해주며, Action을 SubView로 받을 수 있다.
 public class SparkSection {
+    
+    // MARK: - Properties
+    
     public var actions = [SparkAction]()
+    
+    // MARK: - Initializer
     
     public init() {}
     
-    public func makeStackView() -> UIStackView {
+    // MARK: - Methods
+    
+    /// Section에 맞는 StackView를 반환
+    fileprivate func makeStackView() -> UIStackView {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 0
@@ -26,7 +35,8 @@ public class SparkSection {
         return stack
     }
     
-    public func makeSectionSpacer() -> UIView {
+    /// Section 사이에 빈 공간을 추가하는 UIView 반환
+    fileprivate func makeSectionSpacer() -> UIView {
         let spacer = UIView()
         spacer.backgroundColor = .clear
         spacer.snp.makeConstraints { make in
@@ -36,9 +46,12 @@ public class SparkSection {
     }
 }
 
-// MARK: SparkAction Class
+// MARK: - SparkAction Class
 
+/// Action :  Section으로부터 만들어진 StackView에 들어가는 Button생성
 public class SparkAction {
+    
+    // MARK: - Properties
     
     public enum SparkActionTitleType {
         case normalTitle
@@ -50,6 +63,8 @@ public class SparkAction {
     public var handler: (() -> Void)?
     public var buttonFont: UIFont?
     
+    // MARK: - Initializer
+    
     public init(_ data: String, titleType: SparkActionTitleType = .normalTitle, buttonFont: UIFont? = nil, handler: (() -> Void)?) {
         self.data = data
         self.buttonType = titleType
@@ -57,6 +72,9 @@ public class SparkAction {
         self.handler = handler
     }
     
+    // MARK: - Methods
+    
+    /// Action Data에 맞는 버튼 반환
     fileprivate func makeButton() -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("\(data)", for: .normal)
@@ -80,6 +98,7 @@ public class SparkAction {
         return button
     }
     
+    /// Action 사이를 구분하는 UIView 반환
     fileprivate func makeActionSpacer() -> UIView {
         let spacer = UIView()
         spacer.backgroundColor = .sparkLightGray
@@ -89,13 +108,21 @@ public class SparkAction {
         return spacer
     }
     
+    // MARK: - objc Methods
+    
     @objc
     private func buttonAction() {
         self.handler?()
     }
 }
 
+// MARK: - SparkActionMainStackView Class
+
+/// SparkActionMainStackView
+/// - Section과 Action에서 만든 StackView와 Button을 SubView로 가지는 StackView
 class SparkActionMainStackView: UIStackView {
+    
+    // MARK: - Initializer
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -107,13 +134,22 @@ class SparkActionMainStackView: UIStackView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
+    
+    /// StackView에 존재하는 SubViews를 모두 제거
     public func reset() {
         self.arrangedSubviews.forEach({ $0.removeFromSuperview() })
     }
     
 }
 
+// MARK: - SparkActionSheet
+
+/// SparkActionSheet
+/// - SparkActionMainStackView로 구성한 커스텀 액션 시트를 띄울 ViewController
 class SparkActionSheet: UIViewController {
+    
+    // MARK: - Properties
     
     private var _sections = [SparkSection]()
     
@@ -128,6 +164,8 @@ class SparkActionSheet: UIViewController {
     
     private lazy var tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGestureDidRecognize(_:)))
     
+    // MARK: - Initializer
+    
     override init(nibName nibNameOrNil: String?,
                   bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -138,6 +176,8 @@ class SparkActionSheet: UIViewController {
         super.init(coder: aDecoder)
         setUI()
     }
+    
+    // MARK: - Life Cycles
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -150,7 +190,10 @@ class SparkActionSheet: UIViewController {
         _sections = [SparkSection]()
     }
     
-    // addAction으로 액션을 추가해준다.
+    // MARK: - Methods
+    
+    /// addAction으로 액션을 추가해준다.
+    /// _section이 비었을 경우 먼저 section을 추가한 다음 action을 추가한다.
     public func addAction(_ action: SparkAction) {
         if let section = _sections.last {
             section.actions.append(action)
@@ -161,10 +204,12 @@ class SparkActionSheet: UIViewController {
         }
     }
     
-    // section은 action 사이에 공간을 만들어 준다.
+    /// addSection으로 섹션을 추가해준다.
     public func addSection(_ section: SparkSection = SparkSection()) {
         _sections.append(section)
     }
+    
+    // MARK: - Objc Methods
     
     @objc
     private func tapGestureDidRecognize(_ gesture: UITapGestureRecognizer) {
@@ -194,25 +239,31 @@ extension SparkActionSheet {
         }
     }
     
+    /// _sections에 존재하는 Section과 action을 기준으로 sparkActionMainStackView를 구성한다.
     private func makeActionSheet() {
         let sections = _sections
         let sectionIndex = (sections.count == 0 ? 0 : sections.count-1)
         
+        // 섹션의 수만큼 stackView를 추가한다.
         for i in 0...sectionIndex {
             let stack = sections[i].makeStackView()
             sparkActionMainStackView.addArrangedSubview(stack)
             
             let actionIndex = (sections[i].actions.count == 0 ? 0 : sections[i].actions.count-1)
+            
+            // 각 섹션에 존재하는 액션의 수만큼 button을 추가한다.
             for j in 0...actionIndex {
                 let button = sections[i].actions[j].makeButton()
                 stack.addArrangedSubview(button)
                 
+                // 마지막 액션이 아니면 spacer를 추가하여 액션 사이를 구분한다.
                 if j != actionIndex {
                     let actionSpacer = sections[i].actions[j].makeActionSpacer()
                     stack.addArrangedSubview(actionSpacer)
                 }
             }
             
+            // 마지막 섹션이 아니면 spacer를 추가해준다.
             if i != sectionIndex {
                 let spacer = sections[i].makeSectionSpacer()
                 sparkActionMainStackView.addArrangedSubview(spacer)
