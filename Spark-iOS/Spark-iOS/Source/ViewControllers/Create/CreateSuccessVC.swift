1b4647507b92ae4a103c01e2ef6a7e944f2acc8a
yangsubinn <cindy010670@gmail.com>
2022-02-28 14:45:21 +0900

    [Design] #310 - 방생성 완료 뷰 레이아웃 구현

--- Spark-iOS/Spark-iOS/Source/ViewControllers/Create/CreateSuccessVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Create/CreateSuccessVC.swift
@@ -17,8 +17,13 @@ class CreateSuccessVC: UIViewController {
     private let guideLabel = UILabel()
     private let copyButton = UIButton()
     private let wantImageView = UIImageView()
-    private let homeButton = BottomButton()
-    private let enterButtno = BottomButton()
+    private let homeButton = BottomButton().setUI(.white).setTitle("홈으로 가기")
+    private let enterButton = BottomButton().setUI(.pink).setTitle("대기방 입장하기")
+    
+    var photoOnly: Bool?
+    var roomName: String?
+    var roomCode: String?
+    var roomId: Int?
     
     // MARK: - Life Cycles
     
@@ -32,16 +37,76 @@ class CreateSuccessVC: UIViewController {
     // MARK: - Custom Methods
     
     private func setUI() {
+        titleLabel.text = "30분 독서\n방을 만들었어요!"
+        titleLabel.font = .h2Title
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .sparkPinkred
+        titleLabel.partColorChange(targetString: "방을 만들었어요!", textColor: .sparkBlack)
+        titleLabel.numberOfLines = 2
         
-    }
-    
-    private func setLayout() {
+        guideLabel.text = "코드를 복사해 친구들을 초대해 보세요!"
+        guideLabel.font = .p1TitleLight
+        guideLabel.textAlignment = .center
+        guideLabel.textColor = .sparkDarkGray
         
+        copyButton.setImage(UIImage(named: "btnSmall"), for: .normal)
+        
+        wantImageView.backgroundColor = .gray
     }
     
     private func setAddTarget() {
-        
+        copyButton.addTarget(self, action: #selector(copyToClipboard), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc
+    @objc
+    private func copyToClipboard() {
+        UIPasteboard.general.string = roomCode
+        showToast(x: 20, y: view.safeAreaInsets.top, message: "코드를 복사했어요!", font: .p1TitleLight)
     }
 }
 
 // MARK: - Network
+
+
+// MARK: - Layout
+extension CreateSuccessVC {
+    private func setLayout() {
+        view.addSubviews([titleLabel, guideLabel, copyButton,
+                         wantImageView, homeButton, enterButton])
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(68)
+        }
+        
+        guideLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+        }
+        
+        copyButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(guideLabel.snp.bottom).offset(20)
+            make.width.equalTo(87)
+            make.height.equalTo(36)
+        }
+        
+        wantImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(300)
+            make.top.equalTo(copyButton.snp.bottom).offset(28)
+        }
+        
+        enterButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        homeButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(enterButton.snp.top).offset(-16)
+        }
+    }
+    
+}
