f2dce658ea16e64f4122f771a952327225e1ed2d
hyungyu Kim <ypsophila97@icloud.com>
2022-03-02 11:58:30 +0900

    [Feat] #326 - 킹피셔 리사이징 된 이미지를 가져올 수 있도록 URL 가공

--- Spark-iOS/Spark-iOS/Source/Extensions/UIImageView+.swift
+++ Spark-iOS/Spark-iOS/Source/Extensions/UIImageView+.swift
@@ -10,16 +10,48 @@ import UIKit
 
 import Kingfisher
 
+@frozen
+enum ResizingImagetype {
+    case small
+    case medium
+    case large
+}
+
 extension UIImageView {
+    /// URL 주소를 가지고 이미지 다운로드.
+    ///
+    /// type: 리사이징된 이미지를 설정할 수 있음.
+    /// - small - 270*270
+    /// - medium - 360*360
+    /// - large - 720*720
     @discardableResult
-    func updateImage(_ imagePath: String) -> Bool {
-        guard let url = URL(string: imagePath) else {
+    func updateImage(_ imagePath: String, type: ResizingImagetype = .small) -> Bool {
+        guard let periodIndex = imagePath.lastIndex(of: ".") else {
+            self.image = UIImage()
+            return false
+        }
+        let imageURL = imagePath[imagePath.startIndex..<periodIndex]
+        // .png or .jpeg
+        let imageType = imagePath[periodIndex..<imagePath.endIndex]
+
+        let resizingURL: String
+        switch type {
+        case .small:
+            resizingURL = imageURL + "_270x270" + imageType
+        case .medium:
+            resizingURL = imageURL + "_360x360" + imageType
+        case .large:
+            resizingURL = imageURL + "_720x720" + imageType
+        }
+        
+        guard let url = URL(string: resizingURL) else {
             self.image = UIImage()
             return false
         }
         self.kf.indicatorType = .none
         self.kf.setImage(
             with: url,
+            placeholder: UIImage(named: "placeholder"),
             options: [
                 .scaleFactor(UIScreen.main.scale),
                 .transition(.fade(0.3)),
@@ -35,4 +67,3 @@ extension UIImageView {
         return true
     }
 }
-
