0754f01c9360bbd0d41aefe8c5a39a51491007b9
hyungyu Kim <ypsophila97@icloud.com>
2022-03-04 21:10:59 +0900

    [Chore] #342 - 프로필 수정 뷰컨, 스토리보드 추가

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/EditProfileVC.swift
@@ -0,0 +1,29 @@
+//
+//  EditProfileVC.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/03/04.
+//
+
+import UIKit
+
+class EditProfileVC: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+    }
+}
+
+// MARK: - Extension
+
+extension EditProfileVC {
+    private func setUI() {
+        
+    }
+}
