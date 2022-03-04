b856bc3363fd54944aabf6413707e8202c0baddd
hyungyu Kim <ypsophila97@icloud.com>
2022-03-04 23:08:19 +0900

    [Refactor] #342 - changeCountLabel 파라미터 중 lineView 삭제
    - textFieldShouldBeginEditing 에서 라인뷰를 이미 활성화하고 있기때문에 불필요한 파라미터

--- Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/EditProfileVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/EditProfileVC.swift
@@ -11,19 +11,246 @@ class EditProfileVC: UIViewController {
 
     // MARK: - Properties
     
+    private var customNavigationBar = LeftButtonNavigaitonBar()
+    private let profileImageView = UIImageView()
+    private let fadeView = UIView()
+    private let photoIconImageView = UIImageView()
+    private let textField = UITextField()
+    private let lineView = UIView()
+    private let countLabel = UILabel()
+    private var completeButton = BottomButton().setUI(.pink).setTitle("수정 완료").setDisable()
+    private let picker = UIImagePickerController()
+    private let tap = UITapGestureRecognizer()
+    private let maxLength: Int = 10
+    
+    var profileImage: UIImage?
+    var nickname: String?
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUI()
+        setLayout()
+        setNotification()
+        setAddTarget()
+        setDelegate()
+    }
+}
+    
+    // MARK: - Extension
+extension EditProfileVC {
+    private func setUI() {
+        customNavigationBar.title("프로필 수정")
+            .leftButtonImage("icQuit")
+            .leftButonAction {
+                self.dismiss(animated: true, completion: nil)
+            }
+        
+        profileImageView.backgroundColor = .blue
+        profileImageView.image = profileImage
+        profileImageView.layer.cornerRadius = 58
+        profileImageView.layer.masksToBounds = true
+        
+        fadeView.backgroundColor = .sparkBlack.withAlphaComponent(0.6)
+        fadeView.layer.cornerRadius = 58
+        fadeView.addGestureRecognizer(tap)
+        
+        photoIconImageView.image = UIImage(named: "icImage")
+        
+        textField.borderStyle = .none
+        textField.placeholder = nickname
+        textField.tintColor = .sparkPinkred
+        
+        lineView.backgroundColor = .sparkGray
+        
+        countLabel.text = "0/10"
+        countLabel.font = .captionEng
+        countLabel.textColor = .sparkDarkGray
+    }
+    
+    private func setDelegate() {
+        textField.delegate = self
+        picker.delegate = self
+    }
+    
+    private func setNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange(_:)), name: UITextField.textDidChangeNotification, object: nil)
+    }
+    
+    private func setAddTarget() {
+        completeButton.addTarget(self, action: #selector(touchCompleteButton), for: .touchUpInside)
+        tap.addTarget(self, action: #selector(showAlter))
+    }
+    
+    private func openLibrary() {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    private func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        }
+    }
+    
+    // MARK: - @objc
+    
+    @objc
+    private func textFieldDidChange(_ notification: Notification) {
+        if let textField = notification.object as? UITextField {
+            self.changeCountLabel(textField: textField, maxLength: maxLength, countLabel: countLabel)
+        }
+    }
+    
+    @objc
+    func showAlter() {
+        let alert = SparkActionSheet()
+        
+        alert.addAction(SparkAction("카메라 촬영", titleType: .blackMediumTitle, handler: {
+            alert.dismiss(animated: true) {
+                self.openCamera()
+            }
+        }))
+        
+        alert.addAction(SparkAction("앨범에서 선택", titleType: .blackMediumTitle, handler: {
+            alert.dismiss(animated: true) {
+                self.openLibrary()
+            }
+        }))
+        
+        if profileImageView.image != UIImage(named: "profileEmpty") {
+            alert.addAction(SparkAction("사진 삭제", titleType: .blackMediumTitle, handler: {
+                self.profileImageView.image = UIImage(named: "profileEmpty")
+            }))
+        }
+        
+        alert.addSection()
+        
+        alert.addAction(SparkAction("취소", titleType: .blackBoldTitle, handler: {
+            alert.animatedDismiss(completion: nil)
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    @objc
+    func touchCompleteButton() {
+        if profileImageView.image == UIImage(named: "profileEmpty") {
+//            signupWithAPI(profileImg: nil, nickname: textField.text ?? "") {
+//                self.dismiss(animated: true, completion: nil)
+//            }
+        } else {
+//            signupWithAPI(profileImg: profileImageView.image ?? UIImage(), nickname: textField.text ?? "") {
+//                self.dismiss(animated: true, completion: nil)
+//            }
+        }
     }
 }
 
-// MARK: - Extension
+// MARK: - UITextFieldDelegate
+
+extension EditProfileVC: UITextFieldDelegate {
+    // 여백 클릭 시
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // 리턴 눌렀을 때
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    // 입력 시작
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        lineView.backgroundColor = .sparkPinkred
+        return true
+    }
+    
+    // 입력 끝
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.hasText {
+            completeButton.setAble()
+            lineView.backgroundColor = .sparkPinkred
+        } else {
+            completeButton.setDisable()
+            lineView.backgroundColor = .sparkGray
+        }
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension EditProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // UIImage 타입인 originalImage를 빼옴
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImageView.image = image.resize(newWidth: profileImageView.frame.width)
+            profileImageView.contentMode = .scaleAspectFill
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Network
 
 extension EditProfileVC {
-    private func setUI() {
+    
+}
+
+// MARK: - Layout
+
+extension EditProfileVC {
+    private func setLayout() {
+        view.addSubviews([customNavigationBar, profileImageView,
+                          fadeView, textField, lineView,
+                          countLabel, completeButton])
         
+        customNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(customNavigationBar.snp.bottom).offset(128)
+            make.width.height.equalTo(115)
+        }
+        
+        fadeView.addSubview(photoIconImageView)
+        
+        fadeView.snp.makeConstraints { make in
+            make.edges.equalTo(profileImageView)
+        }
+        
+        photoIconImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(55)
+            make.leading.equalToSuperview().inset(36)
+            make.width.equalTo(290)
+            make.height.equalTo(46)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(textField.snp.centerY)
+            make.trailing.equalToSuperview().inset(28)
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(28)
+            make.top.equalTo(textField.snp.bottom)
+            make.height.equalTo(2)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
     }
 }
