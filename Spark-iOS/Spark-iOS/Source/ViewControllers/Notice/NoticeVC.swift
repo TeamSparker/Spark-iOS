51679466acbca0050eba2b885c548ce374fef4e0
yangsubinn <cindy010670@gmail.com>
2022-03-09 03:35:46 +0900

    [Design] #348 - collectionView inset 추가

--- Spark-iOS/Spark-iOS/Source/ViewControllers/Notice/NoticeVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Notice/NoticeVC.swift
@@ -220,6 +220,10 @@ extension NoticeVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 21, right: 0)
+    }
 }
 
 // MARK: - Network
