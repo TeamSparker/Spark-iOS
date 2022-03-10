510829a9f18623ec92f508f024fa13e9e43a9ee0
yangsubinn <cindy010670@gmail.com>
2022-03-10 17:47:08 +0900

    [Add] #364 - ActiveNotice 추가

--- Spark-iOS/Spark-iOS/Source/NetworkServices/Notice/NoticeAPI.swift
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/Notice/NoticeAPI.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+import Moya
+
+public class NoticeAPI {
+    
+    static let shared = NoticeAPI()
+    var noticeProvider = MoyaProvider<NoticeService>(plugins: [MoyaLoggerPlugin()])
+    
+    public init() { }
+    
+//    func activeFetch(lastID: Int, size: Int, completion: @escaping(NetworkResult<Any>) -> Void) {
+//
+//    }
+}
