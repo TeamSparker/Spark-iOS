108b2e0b2f300ca1b58c3f7db56d5241cd98a351
hyungyu Kim <ypsophila97@icloud.com>
2022-03-12 02:04:00 +0900

    [Add] #375 - 습관방 성공/실패 다이얼로그 파일 추가

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Dialogue/CompleteFailDialogueVC.swift
@@ -0,0 +1,29 @@
+//
+//  CompleteFailDialogueVC.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/03/12.
+//
+
+import UIKit
+
+class CompleteFailDialogueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
