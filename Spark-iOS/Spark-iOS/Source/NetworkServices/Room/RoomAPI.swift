26cc9e05bd5705d53a124ba635241d75026daf8c
hyungyu Kim <ypsophila97@icloud.com>
2022-03-12 14:35:03 +0900

    [Feat] #379 - 성공/실패 습관티켓 읽음 처리 서버 연결

--- Spark-iOS/Spark-iOS/Source/NetworkServices/Room/RoomAPI.swift
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/Room/RoomAPI.swift
@@ -330,4 +330,18 @@ public class RoomAPI {
             }
         }
     }
+    
+    func readRoom(roomID: Int, completion: @escaping(NetworkResult<Any>) -> Void) {
+        roomProvider.request(.readRoom(roomID: roomID)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
 }
