a2f65f50f95dc164f027572c0ecc66363908c3f0
hyun99999 <ypsophila97@icloud.com>
2022-03-14 14:45:24 +0900

    [Comment] modify remark

--- Spark-iOS/Spark-iOS/Source/Classes/SparkTabBar/SparkTabBarItem.swift
+++ Spark-iOS/Spark-iOS/Source/Classes/SparkTabBar/SparkTabBarItem.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 
 /// Implement the button on the tab bar.
-/// - consist of UIImageView and UILabel.
+/// - consist of UIImageView, UILabel, and UIView.
 final class SparkTabBarItem: UIView {
     
     // MARK: - Properties
@@ -18,20 +18,23 @@ final class SparkTabBarItem: UIView {
     private var imageView = UIImageView()
     private var titleLable = UILabel()
     
+    /// Whether spark tab bar item is selected.
     var isSelected = false {
         didSet {
             reloadAppearnce()
         }
     }
     
-    // default selected color is spark black.
+    /// Spark tab bar item's selected color.
+    /// - default selected color is spark black.
     var selectedColor: UIColor = .sparkBlack {
         didSet {
             reloadAppearnce()
         }
     }
     
-    // default deselected color is spark dark gray.
+    /// Spark tab bar item's deselected color.
+    // - default deselected color is spark dark gray.
     var deselectedColor: UIColor = .sparkDarkGray {
         didSet {
             reloadAppearnce()
@@ -53,6 +56,9 @@ final class SparkTabBarItem: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
+    
+    /// Set UI.
     private func setUI() {
         self.backgroundColor = .clear
         self.imageView.contentMode = .scaleAspectFit
@@ -60,27 +66,28 @@ final class SparkTabBarItem: UIView {
         titleLable.font = .caption
     }
     
-    /// set spark tab bar button with UITabBarItem.
+    /// Set spark tab bar button with UITabBarItem.
     private func setItem(image: UIImage?, title: String?) {
         if let image = image, let title = title {
-            imageView.image = image.withRenderingMode(.alwaysOriginal)
+            imageView.image = image.withRenderingMode(.alwaysTemplate)
             titleLable.text = title
         }
     }
     
-    /// reload tint color.
+    /// Reload spark tab bar item's color.
     private func reloadAppearnce() {
         self.tintColor = isSelected ? selectedColor : deselectedColor
-        // FIXME: - 혹시 여기서 이미지뷰랑 텍스트 색도 변경해줘야하나? 틴트컬러만 세팅해주면되나..?
+        self.titleLable.textColor = isSelected ? selectedColor : deselectedColor
     }
     
-    /// called when spark tab bar is selected.
+    /// Called when spark tab bar is selected.
     func itemIsSelected() {
         isSelected = isSelected ? true : false
     }
     
     // MARK: - Layout
     
+    /// Set layout.
     private func setLayout() {
         self.addSubviews([imageView, titleLable])
         
@@ -92,6 +99,7 @@ final class SparkTabBarItem: UIView {
         titleLable.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom).offset(1)
             $0.bottom.equalToSuperview()
+            $0.centerX.equalTo(imageView.snp.centerX)
         }
     }
 }
