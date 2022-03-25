33f83d7d1535739762072c03dab1e78e206b88e6
hyun99999 <ypsophila97@icloud.com>
2022-03-25 15:02:47 +0900

    [Add] 회원탈퇴 뷰컨트롤러, 스토리보드 추가

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/WithdrawalVC.swift
@@ -0,0 +1,29 @@
+//
+//  WithdrawalVC.swift
+//  Spark-iOS
+//
+//  Created by kimhyungyu on 2022/03/25.
+//
+
+import UIKit
+
+class WithdrawalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
