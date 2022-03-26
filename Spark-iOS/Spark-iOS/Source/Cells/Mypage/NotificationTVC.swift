de8c6fc5c3c24fdd15088967e15108a92cf54f03
hyun99999 <ypsophila97@icloud.com>
2022-03-27 05:56:20 +0900

    [Feat] #384 - 알림뷰 셀 구현

--- Spark-iOS/Spark-iOS/Source/Cells/Mypage/NotificationTVC.swift
+++ Spark-iOS/Spark-iOS/Source/Cells/Mypage/NotificationTVC.swift
@@ -9,15 +9,72 @@ import UIKit
 
 class NotificationTVC: UITableViewCell {
 
+    // MARK: - Properties
+    
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    private let switchControl = UISwitch()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setUI()
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = ""
+        subtitleLabel.text = ""
+        switchControl.isOn = false
     }
+}
 
+extension NotificationTVC {
+    private func setUI() {
+        titleLabel.font = .p1Title
+        titleLabel.textColor = .sparkBlack
+        
+        subtitleLabel.font = .caption
+        subtitleLabel.textColor = .sparkDarkGray
+        
+        switchControl.isOn = false
+    }
+    
+    // cell initializer.
+    public func initCell(with type: NotificationTableRow, isOn: Bool) {
+        switch type {
+        case .roomStart:
+            titleLabel.text = "습관방 시작"
+            subtitleLabel.text = "대기 중이었던 방이 시작되면 바로 알 수 있어요."
+            switchControl.isOn = isOn
+        case .spark:
+            titleLabel.text = "스파크 보내기"
+            subtitleLabel.text = "받은 스파크를 확인할 수 있어요."
+            switchControl.isOn = isOn
+        case .consider:
+            titleLabel.text = "고민중"
+            subtitleLabel.text = "다른 스파커가 고민중인 경우 바로 알 수 있어요."
+            switchControl.isOn = isOn
+        case .certification:
+            titleLabel.text = "인증 완료"
+            subtitleLabel.text = "스파커들의 인증 사진을 바로 확인할 수 있어요."
+            switchControl.isOn = isOn
+        case .remind:
+            titleLabel.text = "미완료 습관방"
+            subtitleLabel.text = "21:00에 미완료된 방이 있을 경우 알 수 있어요."
+            switchControl.isOn = isOn
+        }
+    }
+}
+
+// MARK: - Layout
+
+extension NotificationTVC {
+    private func setLayout() {
+        self.contentView.addSubviews([titleLabel, subtitleLabel, switchControl])
+        
+        
+    }
 }
