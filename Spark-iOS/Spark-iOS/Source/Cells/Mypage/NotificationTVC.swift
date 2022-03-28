7fabe11f35f821368be5445f66559992f4cd6e01
hyun99999 <ypsophila97@icloud.com>
2022-03-28 18:13:56 +0900

    [Feat] #384 - 알림뷰 셀 구현

--- Spark-iOS/Spark-iOS/Source/Cells/Mypage/NotificationTVC.swift
+++ Spark-iOS/Spark-iOS/Source/Cells/Mypage/NotificationTVC.swift
@@ -13,14 +13,21 @@ class NotificationTVC: UITableViewCell {
     
     private let titleLabel = UILabel()
     private let subtitleLabel = UILabel()
-    private let notificationControl = UISwitch()
+    private let notificationSwitch = UISwitch()
     private let lineView = UIView()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    // MARK: - View Life Cycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setUI()
+        setAddTargets()
         setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func prepareForReuse() {
@@ -28,7 +35,7 @@ class NotificationTVC: UITableViewCell {
         
         titleLabel.text = ""
         subtitleLabel.text = ""
-        notificationControl.isOn = false
+        notificationSwitch.isOn = false
         lineView.isHidden = false
     }
 }
@@ -41,37 +48,47 @@ extension NotificationTVC {
         subtitleLabel.font = .caption
         subtitleLabel.textColor = .sparkDarkGray
         
-        notificationControl.isOn = false
+        notificationSwitch.isOn = false
         
         lineView.backgroundColor = .sparkGray
     }
     
     // cell initializer.
     public func initCell(with type: NotificationTableRow, isOn: Bool) {
-        notificationControl.isOn = isOn
+        notificationSwitch.isOn = isOn
         
         switch type {
         case .roomStart:
             titleLabel.text = "습관방 시작"
             subtitleLabel.text = "대기 중이었던 방이 시작되면 바로 알 수 있어요."
-            lineView.isHidden = true
+            lineView.isHidden = false
         case .spark:
             titleLabel.text = "스파크 보내기"
             subtitleLabel.text = "받은 스파크를 확인할 수 있어요."
-            lineView.isHidden = false
+            lineView.isHidden = true
         case .consider:
             titleLabel.text = "고민중"
             subtitleLabel.text = "다른 스파커가 고민중인 경우 바로 알 수 있어요."
-            lineView.isHidden = false
+            lineView.isHidden = true
         case .certification:
             titleLabel.text = "인증 완료"
             subtitleLabel.text = "스파커들의 인증 사진을 바로 확인할 수 있어요."
-            lineView.isHidden = true
+            lineView.isHidden = false
         case .remind:
             titleLabel.text = "미완료 습관방"
             subtitleLabel.text = "21:00에 미완료된 방이 있을 경우 알 수 있어요."
-            lineView.isHidden = false
+            lineView.isHidden = true
         }
+    }
+    
+    private func setAddTargets() {
+        notificationSwitch.addTarget(self, action: #selector(touchNotificationSwitch), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc
+    private func touchNotificationSwitch() {
     }
 }
 
@@ -79,7 +96,7 @@ extension NotificationTVC {
 
 extension NotificationTVC {
     private func setLayout() {
-        self.contentView.addSubviews([titleLabel, subtitleLabel, notificationControl])
+        self.contentView.addSubviews([titleLabel, subtitleLabel, notificationSwitch, lineView])
         
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(11)
@@ -91,11 +108,11 @@ extension NotificationTVC {
             $0.leading.equalTo(titleLabel.snp.leading)
         }
         
-        notificationControl.snp.makeConstraints {
+        notificationSwitch.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(30)
             $0.width.equalTo(51)
-            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.top.equalTo(titleLabel.snp.top).offset(10)
         }
         
         lineView.snp.makeConstraints {
--- Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/NotificationVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/Mypage/NotificationVC.swift
@@ -9,6 +9,22 @@ import UIKit
 
 import SnapKit
 
+@frozen
+public enum NotificationTableSection: Int, CaseIterable {
+    case information
+    case sparkerActivity
+    case remind
+}
+
+@frozen
+public enum NotificationTableRow: Int {
+    case roomStart      // 습관방 시작
+    case spark          // 스파크 보내기
+    case consider       // 고민중
+    case certification  // 인증 완료
+    case remind         // 미완료 습관방
+}
+
 class NotificationVC: UIViewController {
     
     // MARK: - Properties
@@ -20,7 +36,7 @@ class NotificationVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUI()
         setTableView()
         setLayout()
@@ -41,8 +57,7 @@ extension NotificationVC {
     private func setTableView() {
         tableView.delegate = self
         tableView.dataSource = self
-        
-        tableView.register(NotificationHeaderTVC.self, forCellReuseIdentifier: Const.Cell.Identifier.notificationHeaderTVC)
+
         tableView.register(NotificationTVC.self, forCellReuseIdentifier: Const.Cell.Identifier.notificationTVC)
         
         tableView.showsVerticalScrollIndicator = false
@@ -58,18 +73,85 @@ extension NotificationVC {
 // MARK: - UITableViewDelegate
 
 extension NotificationVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let section = NotificationTableSection(rawValue: indexPath.section) else { return 0}
+        switch section {
+        case .information:
+            return 84
+        case .sparkerActivity, .remind:
+            return 80
+        }
+    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 45
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let section = NotificationTableSection(rawValue: section) else { return UIView() }
+        switch section {
+        case .information:
+            return NotificationTableHeaderView(type: .information)
+        case .sparkerActivity:
+            return NotificationTableHeaderView(type: .sparkerActivity)
+        case .remind:
+            return NotificationTableHeaderView(type: .remind)
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 
 extension NotificationVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return NotificationTableSection.allCases.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        var rowOfSection: [NotificationTableRow]
+        guard let section = NotificationTableSection(rawValue: section) else { return 0 }
+        switch section {
+        case .information:
+            rowOfSection = [.roomStart]
+            
+            return rowOfSection.count
+        case .sparkerActivity:
+            
+            rowOfSection = [.spark, .consider, .certification]
+            
+            return rowOfSection.count
+        case .remind:
+            
+            rowOfSection = [.remind]
+            
+            return rowOfSection.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let section = NotificationTableSection(rawValue: indexPath.section) else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Const.Cell.Identifier.notificationTVC, for: indexPath) as? NotificationTVC else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        switch section {
+        case .information:
+            cell.initCell(with: .roomStart, isOn: false)
+            
+            return cell
+        case .sparkerActivity:
+            /*
+             NotificationTableRow(rawValue: 1) 는 .spark 이다.
+             NotificationTableRow(rawValue: 2) 는 .consider 이다.
+             NotificationTableRow(rawValue: 3) 는 .certification 이다.
+             */
+            guard let row = NotificationTableRow(rawValue: indexPath.section + indexPath.row) else { return UITableViewCell() }
+            cell.initCell(with: row, isOn: false)
+            
+            return cell
+        case .remind:
+            cell.initCell(with: .remind, isOn: false)
+            
+            return cell
+        }
     }
 }
 
