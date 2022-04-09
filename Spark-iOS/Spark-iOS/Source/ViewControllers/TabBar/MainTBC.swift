c147eea34bed70c821935f60a444f557bc351493
hyun99999 <ypsophila97@icloud.com>
2022-04-09 17:53:40 +0900

    [Fix] #508 - 플로팅버튼 독립적으로 터치되도록 수정

--- Spark-iOS/Spark-iOS/Source/ViewControllers/TabBar/MainTBC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/TabBar/MainTBC.swift
@@ -78,6 +78,7 @@ extension MainTBC {
         floatingButton.layer.shadowRadius = CGFloat(10)
         floatingButton.buttonDiameter = 60.0
         floatingButton.itemSizeRatio = 44.0 / 60.0
+        floatingButton.isExclusiveTouch = true
         
         // set floatingButton items.
         floatingButton.addItem(title: "코드로 참여", image: UIImage(named: "icCode")) { _ in
