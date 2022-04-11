a5994a05b1965a9d255a28b182215fa20935f817
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-11 17:29:14 +0900

    [Feat] #470 - 대표이미지 변경 뷰에서 none 및 rest data 필터링
    대표이미지 변경 뷰에서 none 및 rest data 필터링

--- Spark-iOS/Spark-iOS/Source/ViewControllers/StorageMore/StorageMoreVC.swift
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/StorageMore/StorageMoreVC.swift
@@ -262,7 +262,12 @@ extension StorageMoreVC {
             switch response {
             case .success(let data):
                 if let myRoomCerti = data as? MyRoomCertification {
-                    self.myRoomCertificationList?.append(contentsOf: myRoomCerti.records ?? [])
+                    if (self.isChangingImageView) {
+                        let filteredData = myRoomCerti.records?.filter({ $0.status == "DONE" })
+                        self.myRoomCertificationList?.append(contentsOf: filteredData ?? [])
+                    } else {
+                        self.myRoomCertificationList?.append(contentsOf: myRoomCerti.records ?? [])
+                    }
                     self.storageMoreCV.reloadData()
                 }
                 completion()
