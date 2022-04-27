8cdffc6edc3a9a546142514e27073e976ffbb98d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-27 17:53:47 +0900

    [Feat] #572 - 상수 추가
    상수 추가

--- Spark-iOS/Spark-iOS/Resource/Constants/Tracking.swift
+++ Spark-iOS/Spark-iOS/Resource/Constants/Tracking.swift
@@ -19,5 +19,9 @@ struct Tracking {
         static let clickNextCreateRoom = "click_NEXT_create_room"
         static let clickUpload = "click_UPLOAD"
         static let clickStartHabit = "click_START_HABIT"
+        static let clickOK = "click_OK_input_code"
+        static let clickFeed = "click_FEED"
+        static let clickShare = "click_SHARE_INSTAGRAM"
+        static let clickCard = "click_CARD_my_room"
     }
 }
