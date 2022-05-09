637353bfff55f2a366b9598f0d6fc3e81ae3ee0d
hyun99999 <ypsophila97@icloud.com>
2022-05-10 02:03:06 +0900

    [Feat] #603 - 이미 인증한 사용자에게 스파크보내기를 할 경우 440 에러코드를 넘겨줌

--- Spark-iOS/Spark-iOS/Source/NetworkServices/Room/RoomAPI.swift
+++ Spark-iOS/Spark-iOS/Source/NetworkServices/Room/RoomAPI.swift
@@ -216,12 +216,28 @@ public class RoomAPI {
             case .success(let response):
                 let statusCode = response.statusCode
                 let data = response.data
-                let networkResult = self.judgeStatus(by: statusCode, data)
+                let networkResult = self.judgeSendSparkStatus(by: statusCode, data)
                 completion(networkResult)
                 
             case .failure(let err):
                 print(err)
             }
+        }
+    }
+    
+    private func judgeSendSparkStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<String>.self, from: data)
+        else { return .pathErr }
+        switch statusCode {
+        case 200:
+            return .success(decodedData.message)
+        case 400..<500:
+            return .requestErr(decodedData.status)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
         }
     }
     
