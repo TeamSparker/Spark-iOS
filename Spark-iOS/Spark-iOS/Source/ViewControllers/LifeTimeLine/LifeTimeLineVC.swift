878b03f13dd1d05366ffe6fbed9563eb94cfebb0
Subin Yang <cindy010670@gmail.com>
2022-10-04 00:07:12 +0900

    [Feat] TimeLineVC 생성, 임시 더보기 버튼 추가 연결

---
+++ Spark-iOS/Spark-iOS/Source/ViewControllers/LifeTimeLine/LifeTimeLineVC.swift
@@ -0,0 +1,29 @@
+//
+//  LifeTimeLineVC.swift
+//  Spark-iOS
+//
+//  Created by 양수빈 on 2022/10/03.
+//
+
+import UIKit
+
+class LifeTimeLineVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
